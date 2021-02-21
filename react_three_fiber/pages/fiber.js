b7fa8a01fd742eaa476f215e431c5523656d627e
Andrew Miller <jazznerd206@icloud.com>
2021-02-21 10:46:55 -0800

    stage and light built

--- react_three_fiber/pages/fiber.js
+++ react_three_fiber/pages/fiber.js
@@ -1,7 +1,20 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { useFrame } from 'react-three-fiber';
+import * as THREE from '../node_modules/three';
 
-function Fiber(props) {
+export function Numbers() {
+    const numbers = useRef();
+    let x = window.innerWidth * .25;
+    let y = window.innerHeight * .5;
+    
+    return(
+        <mesh>
+
+        </mesh>
+    )
+}
+
+export function Box(props) {
   // This reference will give us direct access to the mesh
   const mesh = useRef()
 
@@ -11,21 +24,133 @@ function Fiber(props) {
 
   // Rotate mesh every frame, this is outside of React without overhead
   useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+    mesh.current.rotation.x = mesh.current.rotation.y -= 0.05;
   })
 
   return (
     <mesh
       {...props}
       ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      scale={active ? [1, 1, 1] : [.5, .5, .5]}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}>
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <boxBufferGeometry args={[3, 3, 3]} />
+      <meshStandardMaterial
+        roughness={.5}
+        metalness={1.5}
+        color={hovered ? 'slateblue' : 'green'} />
     </mesh>
   )
 }
 
-export default Fiber;
+export function Light({ brightness, color }) {
+
+    const convertToScale = (oldY, mouseX, mouseY, lightZ) => {
+        // console.log(mouseX, mouseY);
+        let upperWidthBound, upperHeightBound, newX, newY, newZ, newPosition;
+        upperWidthBound = window.innerWidth;
+        upperHeightBound = window.innerHeight;
+        newX = mouseX / upperWidthBound * 100;
+        newY = mouseY / upperHeightBound * 100;
+        newZ = lightZ;
+        oldY < mouseY ? newZ += .05 : newZ -= .05;
+        // console.log(newPosition)
+        newPosition = [newX, newY, newZ];
+        return newPosition
+    }
+
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    useEffect( () => {
+        const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
+        window.addEventListener("mousemove", setFromEvent);
+        let l = light.current.position;
+        console.log('light.current.position ', l);
+        console.log('convert to scale ', convertToScale(l.y, position.x, position.y, l.z));
+        let newLightPosition = convertToScale(l.y, position.x, position.y, l.z)
+        l.set(...newLightPosition);
+        
+        return () => {
+            window.removeEventListener("mousemove", setFromEvent);
+        };
+    }, [position])
+
+    const light = useRef();
+    return (
+      <rectAreaLight
+        ref={light}
+        width={3}
+        height={3}
+        color={color}
+        intensity={brightness}
+        position={[7, 7, 7]}
+        lookAt={[10, 0, 10]}
+        penumbra={1}
+        castShadow
+      />
+    );
+  }
+
+
+// Geometry
+export function GroundPlane() {
+    return (
+      <mesh receiveShadow rotation={[5, 0, 0]} position={[0, -1, -7]}>
+        <planeBufferGeometry attach="geometry" args={[500, 500]} />
+        <meshStandardMaterial attach="material" color="radial-gradient(rgb(255,255,255.25), silver)" />
+      </mesh>
+    );
+  }
+export  function BackDrop() {
+    return (
+      <mesh receiveShadow position={[0, -5, -5]}>
+        <planeBufferGeometry attach="geometry" args={[500, 500]} />
+        <meshStandardMaterial attach="material" color="radial-gradient(silver, rgb(255,255,255.25))" />
+      </mesh>
+    );
+  }
+
+export function FillLight({ brightness, color }) {
+    return (
+      <rectAreaLight
+        width={3}
+        height={3}
+        intensity={brightness}
+        color={color}
+        position={[2, 1, 4]}
+        lookAt={[0, 0, 0]}
+        penumbra={2}
+        castShadow
+      />
+    );
+}
+
+export function RimLight({ brightness, color }) {
+    return (
+      <rectAreaLight
+        width={2}
+        height={2}
+        intensity={brightness}
+        color={color}
+        position={[1, 4, -2]}
+        rotation={[0, 180, 0]}
+        castShadow
+      />
+    );
+}
+
+export function KeyLight({ brightness, color }) {
+    return (
+      <rectAreaLight
+        width={3}
+        height={3}
+        color={color}
+        intensity={brightness}
+        position={[-2, 0, 5]}
+        lookAt={[0, 0, 0]}
+        penumbra={1}
+        castShadow
+      />
+    );
+}
--- react_three_fiber/pages/index.js
+++ react_three_fiber/pages/index.js
@@ -1,15 +1,26 @@
-import Fiber from './fiber';
+import { Box, Light, GroundPlane, BackDrop, FillLight, RimLight, KeyLight, Numbers } from './fiber';
 import { Canvas } from 'react-three-fiber';
 
 export default function Home() {
+
+
   return (
-    <div className="">
+    <div className="App">
       <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Fiber position={[-1.2, 0, 0]} />
-        <Fiber position={[1.2, 0, 0]} />
+        <ambientLight intensity={0.01} />
+        <spotLight position={[0, 1, 25]} angle={0.3} penumbra={.5} />
+        <pointLight position={[0, 0, 0]} args={['white', 2, 10, 5]} />
+        <Light brightness={75} color={"rgb(209,64,9,.25)"} />
+        <Numbers />
+        <Box position={[-3, 0, 0]} />
+        <Box position={[3, 0, 0]} />
+        <KeyLight brightness={25} color="#ffbdf4" />
+        <FillLight brightness={10} color="#F9D71C" />
+        <RimLight brightness={40} color="#FFE484" />
+        <BackDrop />
+        <GroundPlane />
       </Canvas>
     </div>
   )
 }
+
