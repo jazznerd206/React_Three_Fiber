f3cf1b60cbe408923408ffc436e385df1eb9fd67
Andrew Miller <jazznerd206@icloud.com>
2021-02-21 07:50:06 -0800

    canvas is active for three objects

--- react_three_fiber/pages/index.js
+++ react_three_fiber/pages/index.js
@@ -1,7 +1,15 @@
+import Fiber from './fiber';
+import { Canvas } from 'react-three-fiber';
+
 export default function Home() {
   return (
     <div className="">
-      
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Fiber position={[-1.2, 0, 0]} />
+        <Fiber position={[1.2, 0, 0]} />
+      </Canvas>
     </div>
   )
 }
